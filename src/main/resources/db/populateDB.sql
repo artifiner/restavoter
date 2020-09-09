2d28c1b1cd3ca2a36a5ff541e18817ed9ba499eb
artifiner <askovin@yandex.ru>
2020-09-09 20:15:10 +0300

    Add Spring, cache and DB configuration and dependencies

---
+++ src/main/resources/db/populateDB.sql
@@ -0,0 +1,28 @@
+DELETE FROM dishes;
+DELETE FROM votes;
+DELETE FROM restaurants;
+DELETE FROM user_roles;
+DELETE FROM users;
+ALTER SEQUENCE global_seq RESTART WITH 100000;
+
+INSERT INTO users (name, email, password)
+VALUES ('User', 'user@yandex.ru', 'password'),
+       ('Admin', 'admin@gmail.com', 'admin');
+
+INSERT INTO user_roles (role, user_id)
+VALUES ('USER', 100000),
+       ('ADMIN', 100001),
+       ('USER', 100001);
+
+INSERT INTO restaurants (name)
+VALUES ('KFC'),
+       ('Il Patio'),
+       ('Little Osaka');
+
+INSERT INTO dishes (name, price, date_time, restaurant_id)
+VALUES ('Chicken Burger', 5000, '2020-09-08 10:00:00', 100002),
+       ('Wings', 7000, '2020-09-08 10:00:00', 100002),
+       ('Carbonara', 25000, '2020-09-08 10:00:00', 100003),
+       ('Salmon', 45000, '2020-09-08 10:00:00', 100003),
+       ('Sushi', 20000, '2020-09-08 10:00:00', 100004),
+       ('Tom Yam', 30000, '2020-09-08 10:00:00', 100004);
